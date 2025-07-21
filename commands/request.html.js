1e905eb91dc28d8d5046d81b082dfaf2c0cd552d
Adam3553 <156295013+Adam3553@users.noreply.github.com>
2025-07-21 14:26:04 +0530

    Update request.html.js

--- commands/request.html.js
+++ commands/request.html.js
@@ -20,7 +20,7 @@ CMD*/
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Request Game - Hazex Games</title>
+    <title>Request Game - BOSS GAMES</title>
     <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="<% options.CSSFile %>">
